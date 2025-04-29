58de8dfa1a0a8ff4a0915420989e42616bf0ae31
Boughzala Malak <132173773+Gitmalak1@users.noreply.github.com>
2025-04-30 00:09:06 +0100

    hh

---
+++ front/lib/controllers/services_controller.dart
@@ -0,0 +1,30 @@
+import 'package:get/get.dart';
+
+class ServicesController extends GetxController {
+  var services = [
+    {
+      'name': 'حراثة',
+      'image': 'https://img.icons8.com/...tractor.png',
+    },
+    {
+      'name': 'ري',
+      'image': 'https://img.icons8.com/...watering-can.png',
+    },
+    {
+      'name': 'جني',
+      'image': 'https://img.icons8.com/...harvest.png',
+    },
+    {
+      'name': 'تشذيب',
+      'image': 'https://img.icons8.com/...pruning-shears.png',
+    },
+    {
+      'name': 'تسميد',
+      'image': 'https://img.icons8.com/...fertilizer.png',
+    },
+    {
+      'name': 'وقاية نباتات',
+      'image': 'https://img.icons8.com/...pesticide.png',
+    },
+  ].obs;
+}
